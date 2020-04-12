8e01f74061adb91727f527a335a85ea04a67890f
zaid-ali <zaid.ali5430@gmail.com>
2020-04-12 05:02:55 +0500

    added navbar

---
+++ src/components/top-nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar class="top-nav">
+      <b-navbar-brand>
+          Zaid ALi
+      </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+// @ is an alias to /src
+import navbar from '../components/nav'
+
+export default {
+  name: 'top-nav',
+  components: {
+      navbar,
+    
+  }
+}
+</script>
---
+++ src/views/main-board.vue
@@ -0,0 +1,16 @@
+<template>
+  <navbar></navbar>
+</template>
+
+<script>
+// @ is an alias to /src
+import navbar from '../components/top-nav'
+
+export default {
+  name: 'main-board',
+  components: {
+      navbar
+    
+  }
+}
+</script>
